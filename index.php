eef6c26a95a19f97ae9d2e4eb145bb5eee169874
Siphon880gh <wff1012@hotmail.com>
2018-10-09 02:26:50 -0700

    a

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("git-php/Git.php");
 $repo = Git::open('./');
 
 //$output = $repo->run("log --graph --abbrev-commit --decorate --format=format:'%s %C(bold blue)[%H]%C(reset)%n  %C(white)%an%C(reset) %C(dim white) - %aD (%ar)%d%n' --all");
-$output = $repo->run("log --graph --abbrev-commit --decorate --format=format:'%s %C(bold blue)[%H]%C(reset)%n   %C(white)%an%C(reset) %C(dim white) - %aD (%ar)%d' --all");
+$output = $repo->run("log --graph --abbrev-commit --decorate --format=format:'%s %C(bold blue)[%H]%C(reset)%n   %C(white)%an%C(reset) %C(dim white) - %aD (%ar)%d%N' --all");
 $lines = explode("\n", $output);
 
 echo "<pre>"; // preserving tabs to HTML display
@@ -22,6 +22,9 @@ for($i = 0; $i<count($lines); $i++) {
     }
 }
 echo "</pre>"
+
+// Working on Notes
+// https://git-scm.com/docs/git-notes
 
 // Another view:
 // git show-branch
