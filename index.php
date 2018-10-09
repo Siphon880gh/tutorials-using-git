f85c0f18ac2d0e146dc6da8cd6ee402453ddfaeb
Siphon880gh <wff1012@hotmail.com>
2018-10-09 02:15:45 -0700

    testee

--- index.php
+++ index.php
@@ -6,5 +6,39 @@ $repo = Git::open('./');
 
 //echo exec("/usr/local/git/libexec/git-core/git log");
 
-printf($repo->log() . "%s", "hi");
+/* $output = $repo->log();
+$output = str_replace("\n", "<br>", $output);
+echo $output; */
+
+/* $output = $repo->run("log --graph --all --simplify-by-decoration");
+$output = str_replace("\n", "<br>", $output);
+echo $output; */
+
+//$output = $repo->run("log --graph --abbrev-commit --decorate --format=format:'%s %C(bold blue)[%H]%C(reset)%n  %C(white)%an%C(reset) %C(dim white) - %aD (%ar)%d%n' --all");
+$output = $repo->run("log --graph --abbrev-commit --decorate --format=format:'%s %C(bold blue)[%H]%C(reset)%n   %C(white)%an%C(reset) %C(dim white) - %aD (%ar)%d' --all");
+$lines = explode("\n", $output);
+
+echo "<pre>"; // preserving tabs to HTML display
+for($i = 0; $i<count($lines); $i++) {
+    $line = $lines[$i];
+    if($i%2===0) {
+        echo sprintf("%s$line%s", "<span style='color:blue;'>", "</span><br>");
+    } else {
+        $isMatchAfterSymbol = preg_match('/[0-9A-Za-z]/', $line, $matches, PREG_OFFSET_CAPTURE);
+        $strPos = $isMatchAfterSymbol ? $matches[0][1]:-1;
+        if($strPos!==-1) {
+            echo sprintf("%s<span style='color:lightgray;'>%s</span><br>", substr($line, 0, $strPos-1), substr($line, $strPos));
+        }
+    }
+}
+echo "</pre>"
+ 
+/* 
+$output = str_replace("\r", "<br>", $output);
+$output = "<span style='color:blue;'>" . $output;
+$output = str_replace("]]]]", "]</span>", $output);
+
+echo $output; */
+
+// https://github.com/kbjr/Git.php/blob/master/readme.md
 ?>
