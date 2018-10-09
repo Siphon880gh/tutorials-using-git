6e60dbfcb29f2d307b682a4ad0c7d487582b5348
Siphon880gh <wff1012@hotmail.com>
2018-10-09 01:22:42 -0700

    Version 1.0.0

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once("git-php/Git.php");
+$repo = Git::open('./');
+
+//echo exec("/usr/local/git/libexec/git-core/git log");
+
+echo $repo->log();
+?>
